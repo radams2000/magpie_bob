21becc51dcb4f81991091dfc5e22405a9494db00
radams2000 <47715947+radams2000@users.noreply.github.com>
2024-07-28 09:02:13 -0400

    Update main.c

--- main.c
+++ main.c
@@ -50,7 +50,7 @@
 
 // RECORDING TIME IN DMA Blocks (each DMA block is 21.33 ms)
 
-#define RECORDING_TIME_DMABLOCKS 10
+#define RECORDING_TIME_DMABLOCKS 1000
 
 //#define floatingPoint
 
@@ -286,7 +286,7 @@ static q31_t dmaDestBuff_32bit[DMA_buffLen] = {0}; // same data but assembled ba
 //#include "./decimate_test_192k_30dB.txt"
 #endif
 
-volatile q31_t debugBuff[DMA_buffLen] = {0};
+//volatile q31_t debugBuff[DMA_buffLen] = {0};
 
 
 
@@ -913,129 +913,159 @@ void copy_dec_by_2(const q31_t *A, q31_t *B,uint32_t size) {
 
 }
 
-// *************************8 decimate by 8 with halfband IIR allpass decompositions ****
 
-void decimate_8x_iirHB(
+void decimate_8x_iirHB( // takes 1.4ms, new design (7/28/24) with 50dB
 	q31_t * pSrc,
 	q31_t * pDst,
-	uint32_t len)
+	uint32_t len) // note len is the final decimated output length, = input length/8
 {
-	uint32_t k,j1=0;
-	static q31_t ap_state_stage0_zm1=0; // 1st number os decimation stage
-	static q31_t ap_state_stage0_zm0=0;
+	uint32_t k;
+	static q31_t state_stg0_zm1=0; // 1st number os decimation stg
+	static q31_t state_stg0_zm0=0;
 
-	static q31_t ap_state_stage1_zm1=0; // 1st number os decimation stage
-	static q31_t ap_state_stage1_zm0=0;
+	// the non-delayed Allpass state-var paths are designated with _A_
+	// the delayed Allpass state-var paths are designated with _B_
 
-	static q31_t ap_state_stage2_upper_zm2=0;
-	static q31_t ap_state_stage2_upper_zm1=0;
-	static q31_t ap_state_stage2_upper_zm0=0;
+	static q31_t state_stg1_A_zm1=0;
+	static q31_t state_stg1_A_zm0=0;
+	static q31_t state_stg1_B_zm1=0;
+	static q31_t state_stg1_B_zm0=0;
 
-	static q31_t ap_state_stage2_lower_zm1=0;
-	static q31_t ap_state_stage2_lower_zm0=0;
+	static q31_t state_stg2_A_zm2=0;
+	static q31_t state_stg2_A_zm1=0;
+	static q31_t state_stg2_A_zm0=0;
+	static q31_t state_stg2_B_zm1=0;
+	static q31_t state_stg2_B_zm0=0;
 
-	static q31_t deci_stage0_out0 = 0;
-	static q31_t deci_stage0_out1 = 0;
-	static q31_t deci_stage0_out2 = 0;
-	static q31_t deci_stage0_out3 = 0;
+	static q31_t deci_stg0_out0 = 0;
+	static q31_t deci_stg0_out1 = 0;
+	static q31_t deci_stg0_out2 = 0;
+	static q31_t deci_stg0_out3 = 0;
 
-	static q31_t deci_stage1_out0 = 0;
-	static q31_t deci_stage1_out1 = 0;
+	static q31_t deci_stg1_out0 = 0;
+	static q31_t deci_stg1_out1 = 0;
 
-
-	static q31_t coeff_d2_n0_upper = 0x0E4C2F83;
-	static q31_t coeff_d1_n1_upper = 0x78C7E282;
-	static q31_t coeff_d1_n1_lower = 0x39DE69AD;
+	static q31_t coeff_d2_n0_A_stg2 = 0x0E4C2F83;
+	static q31_t coeff_d1_n1_A_stg2 = 0x78C7E282;
+	static q31_t coeff_d1_n1_B_stg2 = 0x39DE69AD;
 	static q31_t mult_temp1;
 	static q31_t mult_temp2;
-	static q31_t allpass_upper,allpass_lower;
+	static q31_t allpass_A,allpass_B;
+
+	static q31_t in0,in1,in2,in3,in4,in5,in6,in7;
+	static q31_t deci_out;
 
 
 
 	k = len;
 	//j0 = 1; // allpass starts with odd inputs and skips by 2
-	j1 = 0; // output pointer into array
-	pSrc+=1; // start looking st position 1 in the array (newer)
+
+
+
+	//pSrc+=1; // start looking st position 1 in the array (newer)
 	while(k > 0) { // each loop produces 2 outputs at 2X rate, so use DMALEN/8
 
-			// 1st shift the zm1 to zm0 based on the current zm1 value
-			ap_state_stage0_zm1 = ap_state_stage0_zm0;
-			ap_state_stage0_zm0 = (*pSrc-- >> 1) - (ap_state_stage0_zm1 >> 2) - (ap_state_stage0_zm1 >> 3); // scale input by 1/2 so the output does not need to be scaled by 1/2
-			//ap_state_stage0_zm0 = (dmaDestBuff_32bit[j0] >> 1) - (ap_state_stage0_zm1 >> 2) - (ap_state_stage0_zm1 >> 3); // scale input by 1/2 so the output does not need to be scaled by 1/2
-//			deci_stage0_out0 = ap_state_stage0_zm1 + (ap_state_stage0_zm0 >> 2) + (ap_state_stage0_zm0 >> 3) + (dmaDestBuff_32bit[j0-1] >> 1);
-			deci_stage0_out0 = ap_state_stage0_zm1 + (ap_state_stage0_zm0 >> 2) + (ap_state_stage0_zm0 >> 3) + (*pSrc >> 1);
-			pSrc+=3;
-			//deci_stage0_out_left[j1++] = deci_stage0_out0 ;
+		in0=(*pSrc++ >> 2);
+		in1=(*pSrc++ >> 2);
+		in2=(*pSrc++ >> 2);
+		in3=(*pSrc++ >> 2);
+		in4=(*pSrc++ >> 2);
+		in5=(*pSrc++ >> 2);
+		in6=(*pSrc++ >> 2);
+		in7=(*pSrc++ >> 2);
 
-			//j0+=2;
-			ap_state_stage0_zm1 = ap_state_stage0_zm0;
-			ap_state_stage0_zm0 = (*pSrc-- >> 1) - (ap_state_stage0_zm1 >> 2) - (ap_state_stage0_zm1 >> 3); // scale input by 1/2 so the output does not need to be scaled by 1/2
-			deci_stage0_out1 = ap_state_stage0_zm1 + (ap_state_stage0_zm0 >> 2) + (ap_state_stage0_zm0 >> 3) + (*pSrc >> 1);
-			pSrc+=3;
 
-			//deci_stage0_out_left[j1++] = deci_stage0_out1 ;
+		// 1st stage comes from 3rd order elliptc prototype, 1st-order in non-delayed branch
+		// ***** 1st 2:1 decimator ***
 
-			//j0+=2;
-			ap_state_stage0_zm1 = ap_state_stage0_zm0;
-			ap_state_stage0_zm0 = (*pSrc-- >> 1) - (ap_state_stage0_zm1 >> 2) - (ap_state_stage0_zm1 >> 3); // scale input by 1/2 so the output does not need to be scaled by 1/2
-			deci_stage0_out2 = ap_state_stage0_zm1 + (ap_state_stage0_zm0 >> 2) + (ap_state_stage0_zm0 >> 3) + (*pSrc >> 1);
-			pSrc+=3;
+		// 1st shift the zm1 to zm0 based on the current zm1 value
+		state_stg0_zm1 = state_stg0_zm0;
+		state_stg0_zm0 = in1 - (state_stg0_zm1 >> 2) - (state_stg0_zm1 >> 4) - (state_stg0_zm1 >> 5); // scale input by 1/2 so the output does not need to be scaled by 1/2
+		deci_stg0_out0 = state_stg0_zm1 + (state_stg0_zm0 >> 2) + (state_stg0_zm0 >> 4) + (state_stg0_zm0 >> 5 )+ in0;
 
-			//deci_stage0_out_left[j1++] = deci_stage0_out2 ;
+		state_stg0_zm1 = state_stg0_zm0;
+		state_stg0_zm0 = in3 - (state_stg0_zm1 >> 2) - (state_stg0_zm1 >> 4) - (state_stg0_zm1 >> 5); // scale input by 1/2 so the output does not need to be scaled by 1/2
+		deci_stg0_out1 = state_stg0_zm1 + (state_stg0_zm0 >> 2) + (state_stg0_zm0 >> 4)+ (state_stg0_zm0 >> 5) + in2;
 
-			//j0+=2;
-			ap_state_stage0_zm1 = ap_state_stage0_zm0;
-			ap_state_stage0_zm0 = (*pSrc-- >> 1) - (ap_state_stage0_zm1 >> 2) - (ap_state_stage0_zm1 >> 3); // scale input by 1/2 so the output does not need to be scaled by 1/2
-			deci_stage0_out3 = ap_state_stage0_zm1 + (ap_state_stage0_zm0 >> 2) + (ap_state_stage0_zm0 >> 3) + (*pSrc >> 1);
-			pSrc+=3;
 
-			//deci_stage0_out_left[j1++] = deci_stage0_out3 ;
+		state_stg0_zm1 = state_stg0_zm0;
+		state_stg0_zm0 = in5 - (state_stg0_zm1 >> 2) - (state_stg0_zm1 >> 4)- (state_stg0_zm1 >> 5); // scale input by 1/2 so the output does not need to be scaled by 1/2
+		deci_stg0_out2 = state_stg0_zm1 + (state_stg0_zm0 >> 2) + (state_stg0_zm0 >> 4)+ (state_stg0_zm0 >> 5)  + in4;
 
-			//j0+=2;
 
-			// at this point I've processed 8 inputs from the dma array and produced 4 outputs
-			// now take 4 outputs and use the same filter as above to produce 2 outputs
-			ap_state_stage1_zm1 = ap_state_stage1_zm0;
-			ap_state_stage1_zm0 = (deci_stage0_out1 >> 1) - (ap_state_stage1_zm1 >> 2) - (ap_state_stage1_zm1 >> 3); // scale input by 1/2 so the output does not need to be scaled by 1/2
-			//deci_stage1_out_left[j1++] = ap_state_stage1_zm1 + (ap_state_stage1_zm0 >> 2) + (ap_state_stage1_zm0 >> 3) + (deci_stage0_out0 >> 1);
-			deci_stage1_out0 = ap_state_stage1_zm1 + (ap_state_stage1_zm0 >> 2) + (ap_state_stage1_zm0 >> 3) + (deci_stage0_out0 >> 1);
-
-			ap_state_stage1_zm1 = ap_state_stage1_zm0;
-			ap_state_stage1_zm0 = (deci_stage0_out3 >> 1) - (ap_state_stage1_zm1 >> 2) - (ap_state_stage1_zm1 >> 3); // scale input by 1/2 so the output does not need to be scaled by 1/2
-			//	deci_stage1_out_left[j1++] = ap_state_stage1_zm1 + (ap_state_stage1_zm0 >> 2) + (ap_state_stage1_zm0 >> 3) + (deci_stage0_out2 >> 1);
-			deci_stage1_out1 = ap_state_stage1_zm1 + (ap_state_stage1_zm0 >> 2) + (ap_state_stage1_zm0 >> 3) + (deci_stage0_out2 >> 1);
-
-			// at this point I've processed 8 inputs from the dma array and produced 2 outputs
-			// now for the final stage, which has a 2nd-order allpass in the un-delayed branch and a 1st-order allpass in the delayed branch
-
-				// 2nd order allpass
-			ap_state_stage2_upper_zm2 = ap_state_stage2_upper_zm1; // non-delayed branch
-			ap_state_stage2_upper_zm1 = ap_state_stage2_upper_zm0;
-			mult_temp1 = (q31_t)  ( ((q63_t) ap_state_stage2_upper_zm1 * coeff_d1_n1_upper) >> 32);
-			mult_temp2 = (q31_t)  ( ((q63_t) ap_state_stage2_upper_zm2 * coeff_d2_n0_upper) >> 32);
-			ap_state_stage2_upper_zm0 = (deci_stage1_out1 >> 1) - mult_temp1 - mult_temp2;
-			mult_temp1 = (q31_t)  ( ((q63_t) ap_state_stage2_upper_zm0 * coeff_d2_n0_upper) >> 32);
-			mult_temp2 = (q31_t)  ( ((q63_t) ap_state_stage2_upper_zm1 * coeff_d1_n1_upper) >> 32);
-		    allpass_upper = mult_temp1 + mult_temp2 + ap_state_stage2_upper_zm2;
-
-		    	// 1st order allpass for the delayed branch
-			ap_state_stage2_lower_zm1 = ap_state_stage2_lower_zm0;
-			mult_temp1 = (q31_t)  ( ((q63_t) ap_state_stage2_lower_zm1 * coeff_d1_n1_lower) >> 32);
-			ap_state_stage2_lower_zm0 = (deci_stage1_out0 >> 1) - mult_temp1;
-			mult_temp1 = (q31_t)  ( ((q63_t) ap_state_stage2_lower_zm0 * coeff_d1_n1_lower) >> 32);
-			allpass_lower = mult_temp1 + ap_state_stage2_lower_zm1;
-
-			*pDst++ =  allpass_lower+allpass_upper;
-			debugBuff[j1++] = allpass_lower+allpass_upper;
+		state_stg0_zm1 = state_stg0_zm0;
+		state_stg0_zm0 = in7 - (state_stg0_zm1 >> 2) - (state_stg0_zm1 >> 4)- (state_stg0_zm1 >> 5); // scale input by 1/2 so the output does not need to be scaled by 1/2
+		deci_stg0_out3 = state_stg0_zm1 + (state_stg0_zm0 >> 2) + (state_stg0_zm0 >> 4)+ (state_stg0_zm0 >> 5)  + in6;
 
 
 
+		// at this point I've processed 8 inputs from the dma array and produced 4 outputs
+		// now take 4 outputs and use the same filter as above to produce 2 outputs
+		// this stage has a 1st-order allpass in both delayed and non-delayed branches (from 5th-order elliptic prototype)
+
+		// ***** 2nd 2:1 decimator ***
+
+		// instance 1
+		// 1st order allpass stg 1, 2 instances to produce 2 outputs
+		// 1st one is non-delayed
+		state_stg1_A_zm1 = state_stg1_A_zm0;
+		state_stg1_A_zm0 = (deci_stg0_out1 >> 1) - (state_stg1_A_zm1 >> 3);
+		allpass_A = state_stg1_A_zm1 + (state_stg1_A_zm0 >> 3);
+			// 1st order allpass for the delayed branch
+		state_stg1_B_zm1 = state_stg1_B_zm0;
+		state_stg1_B_zm0 = (deci_stg0_out0 >> 1) - (state_stg1_B_zm1 >> 1) - (state_stg1_B_zm1 >> 4);
+		allpass_B =  state_stg1_B_zm1 + (state_stg1_B_zm0 >> 1) + (state_stg1_B_zm0 >> 4);
+		deci_stg1_out0 = allpass_B+allpass_A;
+
+		// instance 2
+		state_stg1_A_zm1 = state_stg1_A_zm0;
+		state_stg1_A_zm0 = (deci_stg0_out3 >> 1) - (state_stg1_A_zm1 >> 3);
+		allpass_A = state_stg1_A_zm1 + (state_stg1_A_zm0 >> 3);
+			// 1st order allpass for the delayed branch
+		state_stg1_B_zm1 = state_stg1_B_zm0;
+		state_stg1_B_zm0 = (deci_stg0_out2 >> 1) -(state_stg1_B_zm1 >> 1) - (state_stg1_B_zm1 >> 4);
+		allpass_B = state_stg1_B_zm1 + (state_stg1_B_zm0 >> 1) + (state_stg1_B_zm0 >> 4);
+		deci_stg1_out1 = allpass_B+allpass_A;
 
 
-			k--;
+		// at this point I've processed 8 inputs from the dma array and produced 2 outputs
+		// now for the final stg, which has a 2nd-order allpass in the
+		//un-delayed branch and a 1st-order allpass in the delayed branch (7th order elliptic)
 
-		}
+		// ***** 3rd 2:1 decimator ***
+
+			// 2nd order allpass
+		state_stg2_A_zm2 = state_stg2_A_zm1; // non-delayed branch
+		state_stg2_A_zm1 = state_stg2_A_zm0;
+		mult_temp1 = (q31_t)  ( ((q63_t) state_stg2_A_zm1 * coeff_d1_n1_A_stg2) >> 32);
+		mult_temp2 = (q31_t)  ( ((q63_t) state_stg2_A_zm2 * coeff_d2_n0_A_stg2) >> 32);
+		state_stg2_A_zm0 = (deci_stg1_out1 >> 1) - mult_temp1 - mult_temp2;
+		mult_temp1 = (q31_t)  ( ((q63_t) state_stg2_A_zm0 * coeff_d2_n0_A_stg2) >> 32);
+		mult_temp2 = (q31_t)  ( ((q63_t) state_stg2_A_zm1 * coeff_d1_n1_A_stg2) >> 32);
+		allpass_A = mult_temp1 + mult_temp2 + state_stg2_A_zm2;
+
+			// 1st order allpass for the delayed branch
+		state_stg2_B_zm1 = state_stg2_B_zm0;
+		mult_temp1 = (q31_t)  ( ((q63_t) state_stg2_B_zm1 * coeff_d1_n1_B_stg2) >> 32);
+		state_stg2_B_zm0 = (deci_stg1_out0 >> 1) - mult_temp1;
+		mult_temp1 = (q31_t)  ( ((q63_t) state_stg2_B_zm0 * coeff_d1_n1_B_stg2) >> 32);
+		allpass_B = mult_temp1 + state_stg2_B_zm1;
+
+		deci_out = allpass_B+allpass_A; // this has a gain of 1/4 from the input
+		*pDst++ =  (deci_out << 1) + deci_out; // 20*log10((1/4)*(2 + 1)) = -2.49 dB
+		//debugBuff[j1++] = allpass_B+allpass_A;
+
+
+
+
+
+		k--;
+
+	}
 }
+
+
+
 
 
 // ************************* This is where all the work gets done.
